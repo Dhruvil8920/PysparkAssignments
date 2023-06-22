ebc8341032e6d3d66167307a7cdd8145c4ceb75f
GadoyaDhruvilSanjayb <gadoyadhruvil0@gmail.com>
2023-06-22 16:18:51 +0530

    pyspark assignment trial

---
+++ src/assignment_1/util.py
@@ -0,0 +1,23 @@
+from pyspark.sql import SparkSession
+
+# Create a SparkSession
+spark = SparkSession.builder.getOrCreate()
+
+# Define your data
+data = [("1",), ("2",), ("3",)]  # A list of tuples
+
+# Define the schema
+schema = ["no"]  # A list of column names
+
+
+
+def createDF(data,schema):
+    # Create a DataFrame
+    df = spark.createDataFrame(data, schema)
+    return df
+
+
+def trial(var):
+    pass
+df = createDF(data,schema)
+df.show()
---
+++ test/assignment_1_Test/test.py
@@ -0,0 +1,32 @@
+from src.assignment_1.util import *
+import unittest
+
+data1 = [("1",), ("2",), ("3",)]
+schema1 = ["no"]
+expdf1 = spark.createDataFrame(data1, schema1)
+var = ""
+class TestMyFunc(unittest.TestCase):
+
+    def test_1(self):
+        try:
+            result_df = createDF(data1, schema1)
+            result_str = result_df.show(truncate=False)
+            expected_str = expdf1.show(truncate=False)
+            self.assertEqual(result_str, expected_str)
+            print("test_1 passes")
+
+        except AssertionError:
+            print("test_1 fails")
+            raise
+
+    def test_2(self):
+        try:
+            var_trial = trial(var)
+            pass
+
+        except AssertionError:
+            print("test_1 fails")
+            raise
+
+if __name__ == '__main__':
+    unittest.main()
