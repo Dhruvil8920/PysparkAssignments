32a6ea131f6844a2e73b9535232fa1fdbdaa0a89
GadoyaDhruvilSanjayb <gadoyadhruvil0@gmail.com>
2023-06-23 12:01:47 +0530

    pyspark assignment trial

--- test/assignment_1_Test/test.py
+++ test/assignment_1_Test/test.py
@@ -7,12 +7,12 @@ script_path = os.path.abspath(__file__)
 # Get the directory of the script
 script_directory = os.path.dirname(script_path)
 
-# Get the absolute path of the 'src' directory
-src_directory = os.path.join(script_directory, '..', 'src')
+# Get the absolute path of the parent directory (containing both 'test' and 'src' directories)
+parent_directory = os.path.dirname(script_directory)
 
-# Add the 'src' directory to the Python import path
-sys.path.insert(0, src_directory)
+# Add the parent directory to the Python import path
+sys.path.insert(0, parent_directory)
 
-# Now you can import modules from the 'src' package
-from assignment_1.util import *
+from src.assignment_1.util import *
+
 
