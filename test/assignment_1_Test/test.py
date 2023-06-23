1dfc171bba16112463956c4590eb85126c6a9ef0
GadoyaDhruvilSanjayb <gadoyadhruvil0@gmail.com>
2023-06-23 12:15:00 +0530

    pyspark assignment trial

--- test/assignment_1_Test/test.py
+++ test/assignment_1_Test/test.py
@@ -1,21 +1,3 @@
-import os
-import sys
-
-# Get the absolute path of the current script
-script_path = os.path.abspath(__file__)
-
-# Get the directory containing the script
-script_directory = os.path.dirname(script_path)
-
-# Get the absolute path of the 'src' directory
-src_directory = os.path.join(script_directory, '..', 'src')
-
-# Check if the 'src' directory exists
-if os.path.isdir(src_directory):
-    # Add the 'src' directory to the Python import path
-    sys.path.append(src_directory)
-else:
-    print("Error: 'src' directory not found.")
 
 
 from assignment_1.util import *
