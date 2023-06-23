e01100e59dd6a5fd02c040275ec0e10c800feecf
Meet Parekh <50916854+parekhmeet@users.noreply.github.com>
2023-06-23 13:31:54 +0530

    Update test.py

--- test/assignment_1_Test/test.py
+++ test/assignment_1_Test/test.py
@@ -1,5 +1,5 @@
 import os
 os.chdir("/root/.jenkins/workspace/PysparkAssignmentTest/PysparkAssignments")
-from src.assignment_1.util import *
+from ..src.assignment_1.util import *
 
 print("hello")
