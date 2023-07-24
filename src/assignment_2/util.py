ffb6cebae7ae6eed1ba5cd2addcc6baa8b8d5c28
GadoyaDhruvilSanjayb <gadoyadhruvil0@gmail.com>
2023-07-24 11:16:38 +0530

    pyspark assignment trial

--- src/assignment_2/util.py
+++ src/assignment_2/util.py
@@ -3,26 +3,26 @@ from pyspark.sql import SparkSession
 # Recharge details.
 ''' Use 'date_file.csv' file'''
 # 1.Read the csv file with a custom schema by using the column names hint given in 'date_file.csv'.
-# 2.Create a new column called “changed_Recharge_date” which has Recharge date column date values represented in yyyy-MM-dd default date format.Also change the data type to date type.
+# 2.Create a new column called “changed_Recharge_date” with 'Recharge date' column where the date values represented in yyyyMMdd(integer) change it into default date format (yyyy-MM-dd) with date DataType.
 # 3.drop the ‘RechargeDate’ column.
 # 4.Delete the duplicate rows.
 # 5.Total Count of rows.
 # 6.Add the “RemainingDays” column value to the date part of the column ”changed_Recharge_date” and give the newly calculated date in a column called “expiryDate”.
-# 7.Change the “over” value in ValidityStatus column by a new value called “validity expired”.
+# 7.Change the “over” value in 'ValidityStatus' column by a new value called “validity expired”.
 # 8.Get the count of the column 'expiry_date' filtered based upon the date between "2020-05-12" and "2020-07-11".
 
 #Reading a CSV file and Creating a dataframe.
 def readCSV_to_df(path,schema):
-    a = "hello"
-    return a
+    pass
+    #return df
 
 #Creating new column.
-def create_new_column(df):
+def create_new_column(df, name, datatype):
     pass
     #return df
 
 #Drop the column.
-def drop_column(df):
+def drop_column(df, name):
     pass
     #return df
 
@@ -34,7 +34,7 @@ def remove_duplicates(df):
 #Count the total rows.
 def count_rows(df):
     pass
-    #return df
+    #return count
 
 
 #Creation of expiry Date column.
@@ -42,13 +42,13 @@ def expiry_date(df):
     pass
     #return df
 
-#Changing value in the validity column.
-def change_value(df):
+#Changing value in the validity column (str1=value of column which we need to change, str2=value we need to set).
+def change_value(df, name, str1, str2):
     pass
     #return df
 
 #Filter the column
-def count_date_based_(df):
+def count_date_based_(df, name):
     pass
     #return df
 
