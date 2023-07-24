e18b34a99fed6d2f96016092c465900158a76544
GadoyaDhruvilSanjayb <gadoyadhruvil0@gmail.com>
2023-07-24 11:53:09 +0530

    pyspark assignment trial

--- src/assignment_3/util.py
+++ src/assignment_3/util.py
@@ -7,9 +7,9 @@ from pyspark.sql import SparkSession
 # 1.Write custom schema in the driver program and read the nested json file.
 # 2.Explode the nested Json file into flattened json.
 # 3.Check whether the data in nested json and flattened json are equal.
-# 4.Replace the value of column “imagepaths”  from null to “replaced_val_image_path”.
+# 4.Replace the value of column “imagePaths”  from null to “replaced_val_image_path”.
 # 5.Extract the date value from the column “createdAt” which is in timestamp format.
-# 6.Extract the rows which has the ‘commentComment’ greater than 0.
+# 6.Extract the rows which has the ‘commentCount’ column value greater than 0.
 
 #Read the nested json.
 def readJSON_toDF(path,schema):
@@ -22,18 +22,18 @@ def explode_json(df):
     #return df
 
 #Checking data with source data
-def check_df_data(df):
+def check_df_data(df1, df2):
     pass
     #return df
 
 #Edit column
-def edit_column(df):
+def edit_column(df, colName, value):
     pass
     #return df
 
 
 #Get date from timestamp
-def get_date(df):
+def get_date(df, colName):
     pass
     #return df
 
